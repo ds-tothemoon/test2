05b4e760f7ff5ffed1164f27f603a1e40d9541ec
daeseong.mun <uco.moon@gmail.com>
2024-11-09 09:52:54 +0900

    이미지 원복

--- src/common/photos.jsx
+++ src/common/photos.jsx
@@ -1,137 +1,75 @@
-// import GalleryPhoto0 from "../assets/photo/photo0.jpeg";
-// import GalleryPhoto1 from "../assets/photo/photo1.jpg";
-// import GalleryPhoto2 from "../assets/photo/photo2.png";
-// import GalleryPhoto3 from "../assets/photo/photo3.jpg";
-// import GalleryPhoto4 from "../assets/photo/photo4.jpg";
-// import GalleryPhoto5 from "../assets/photo/photo5.jpg";
-// import GalleryPhoto6 from "../assets/photo/photo6.jpeg";
-// import GalleryPhoto7 from "../assets/photo/photo7.jpeg";
-// import GalleryPhoto8 from "../assets/photo/photo8.jpg";
-// import GalleryPhoto9 from "../assets/photo/photo9.jpg";
-// import GalleryPhoto10 from "../assets/photo/photo10.jpg";
-// import GalleryPhoto11 from "../assets/photo/photo11.jpg";
-// import GalleryPhoto12 from "../assets/photo/photo12.jpeg";
-// import GalleryPhoto13 from "../assets/photo/photo13.jpeg";
-
-// const photos = [
-//   {
-//     original: GalleryPhoto0,
-//     src: GalleryPhoto0,
-//   },
-//   {
-//     original: GalleryPhoto1,
-//     src: GalleryPhoto1,
-//   },
-//   {
-//     original: GalleryPhoto2,
-//     src: GalleryPhoto2,
-//   },
-//   {
-//     original: GalleryPhoto3,
-//     src: GalleryPhoto3,
-//   },
-//   {
-//     original: GalleryPhoto4,
-//     src: GalleryPhoto4,
-//   },
-//   {
-//     original: GalleryPhoto5,
-//     src: GalleryPhoto5,
-//   },
-//   {
-//     original: GalleryPhoto6,
-//     src: GalleryPhoto6,
-//   },
-//   {
-//     original: GalleryPhoto7,
-//     src: GalleryPhoto7,
-//   },
-//   {
-//     original: GalleryPhoto8,
-//     src: GalleryPhoto8,
-//   },
-//   {
-//     original: GalleryPhoto9,
-//     src: GalleryPhoto9,
-//   },
-//   {
-//     original: GalleryPhoto10,
-//     src: GalleryPhoto10,
-//   },
-//   {
-//     original: GalleryPhoto11,
-//     src: GalleryPhoto11,
-//   },
-//   {
-//     original: GalleryPhoto12,
-//     src: GalleryPhoto12,
-//   },
-//   {
-//     original: GalleryPhoto13,
-//     src: GalleryPhoto13,
-//   },
-// ];
-
-const prefix_path = 'https://raw.githubusercontent.com/ds-tothemoon/test2/refs/heads/main/src/assets/photo/'
+import GalleryPhoto0 from "../assets/photo/photo0.jpeg";
+import GalleryPhoto1 from "../assets/photo/photo1.jpg";
+import GalleryPhoto2 from "../assets/photo/photo2.png";
+import GalleryPhoto3 from "../assets/photo/photo3.jpg";
+import GalleryPhoto4 from "../assets/photo/photo4.jpg";
+import GalleryPhoto5 from "../assets/photo/photo5.jpg";
+import GalleryPhoto6 from "../assets/photo/photo6.jpeg";
+import GalleryPhoto7 from "../assets/photo/photo7.jpeg";
+import GalleryPhoto8 from "../assets/photo/photo8.jpg";
+import GalleryPhoto9 from "../assets/photo/photo9.jpg";
+import GalleryPhoto10 from "../assets/photo/photo10.jpg";
+import GalleryPhoto11 from "../assets/photo/photo11.jpg";
+import GalleryPhoto12 from "../assets/photo/photo12.jpeg";
+import GalleryPhoto13 from "../assets/photo/photo13.jpeg";
 
 const photos = [
   {
-    original: prefix_path + 'photo0.jpeg',
-    src: prefix_path + 'photo0.jpeg',
+    original: GalleryPhoto0,
+    src: GalleryPhoto0,
   },
   {
-    original: prefix_path + 'photo1.jpg',
-    src: prefix_path + 'photo1.jpg',
+    original: GalleryPhoto1,
+    src: GalleryPhoto1,
   },
   {
-    original: prefix_path + 'photo2.png',
-    src: prefix_path + 'photo2.png',
+    original: GalleryPhoto2,
+    src: GalleryPhoto2,
   },
   {
-    original: prefix_path + 'photo3.jpg',
-    src: prefix_path + 'photo3.jpg',
+    original: GalleryPhoto3,
+    src: GalleryPhoto3,
   },
   {
-    original: prefix_path + 'photo4.jpg',
-    src: prefix_path + 'photo4.jpg',
+    original: GalleryPhoto4,
+    src: GalleryPhoto4,
   },
   {
-    original: prefix_path + 'photo5.jpg',
-    src: prefix_path + 'photo5.jpg',
+    original: GalleryPhoto5,
+    src: GalleryPhoto5,
   },
   {
-    original: prefix_path + 'photo6.jpeg',
-    src: prefix_path + 'photo6.jpeg',
+    original: GalleryPhoto6,
+    src: GalleryPhoto6,
   },
   {
-    original: prefix_path + 'photo7.jpeg',
-    src: prefix_path + 'photo7.jpeg',
+    original: GalleryPhoto7,
+    src: GalleryPhoto7,
   },
   {
-    original: prefix_path + 'photo8.jpg',
-    src: prefix_path + 'photo8.jpg',
+    original: GalleryPhoto8,
+    src: GalleryPhoto8,
   },
   {
-    original: prefix_path + 'photo9.jpg',
-    src: prefix_path + 'photo9.jpg',
+    original: GalleryPhoto9,
+    src: GalleryPhoto9,
   },
   {
-    original: prefix_path + 'photo10.jpg',
-    src: prefix_path + 'photo10.jpg',
+    original: GalleryPhoto10,
+    src: GalleryPhoto10,
   },
   {
-    original: prefix_path + 'photo11.jpg',
-    src: prefix_path + 'photo11.jpg',
+    original: GalleryPhoto11,
+    src: GalleryPhoto11,
   },
   {
-    original: prefix_path + 'photo12.jpeg',
-    src: prefix_path + 'photo12.jpeg',
+    original: GalleryPhoto12,
+    src: GalleryPhoto12,
   },
   {
-    original: prefix_path + 'photo13.jpeg',
-    src: prefix_path + 'photo13.jpeg',
+    original: GalleryPhoto13,
+    src: GalleryPhoto13,
   },
 ];
-
 
 export default photos;
